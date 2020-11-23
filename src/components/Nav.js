90f5823d8a1fd23d191617b729f1a6be30362e91
Sean Ilagan <seanilagan135@gmail.com>
2020-11-23 14:36:21 +1100

    Made some changes to Nav.js, file is more clean now. Also started working on CSS for the Navbar. Right now it is responsive. Will add a dropdown in the NavBar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,54 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./CSS/Nav.css";
 class Nav extends React.Component {
   render() {
     return (
-      <div>
-        {!this.props.login /* check if a user is logged in  */ ? (
-          <nav>
-            <h3>Logo</h3>
-            <ul>
-              <Link to="/">
-                <li>Home</li>
-              </Link>
-              <Link to="/gradepointaverage">
-                <li>GPA</li>
-              </Link>
-              <Link to="/downloads">
-                <li>Downloads</li>
-              </Link>
-              <Link to="/about">
-                <li>About</li>
-              </Link>
+      <div className = "topnav">
+        <nav>
+          <h3>Logo</h3>
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/gradepointaverage">
+              <li>GPA</li>
+            </Link>
+            <Link to="/downloads">
+              <li>Downloads</li>
+            </Link>
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            {!this.props.login ? (
               <Link to="/login">
                 <li>Login</li>
               </Link>
-            </ul>
-          </nav>
-        ) : (
-          <div>
-            <nav>
-              <h3>Logo</h3>
-              <ul>
-                <Link to="/">
-                  <li>Home</li>
-                </Link>
-                <Link to="/gradepointaverage">
-                  <li>GPA</li>
-                </Link>
-                <Link to="/downloads">
-                  <li>Downloads</li>
-                </Link>
-                <Link to="/about">
-                  <li>About</li>
-                </Link>
-                <Link to="/" onClick={this.props.userLogout}>
-                  <li>Logout</li>
-                </Link>
-              </ul>
-            </nav>
-          </div>
-        )}
+            ) : (
+              <Link to="/" onClick={this.props.userLogout}>
+                <li>Logout</li>
+              </Link>
+            )}
+          </ul>
+        </nav>
       </div>
     );
   }
